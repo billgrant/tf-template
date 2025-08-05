bb002eb00a52d50e732bfd5430895ea57258ebf2
Bill Grant <39649812+billgrant@users.noreply.github.com>
2025-08-05 11:27:54 -0400

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,5 +1,10 @@
 # Boilerplate Terraform configuration to test the random_pet resource
-resource "random_pet" "pet1" { }
+resource "random_pet" "pet1" {}
 
-# resource "random_pet" "pet2" { }
+# resource "random_pet" "pet2" {}
+
+# module "s3" {
+#   source  = "app.terraform.io/billgrant/s3/aws"
+#   version = "~> 1.1.0"
+# }
 
